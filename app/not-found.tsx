b482e463b699588d1dd34d8b2a44455ff8c3305a
Yusuf Yesilyurt <y.yesilyurt14@gmail.com>
2025-09-30 13:46:57 +0200

    Updated not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { Home } from 'lucide-react';
+import { Chrome as Home } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 
 export default function NotFound() {
